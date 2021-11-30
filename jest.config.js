e2057bf7231616f975131ba33bc7be4458a2ae0c
dasporal <dasporal@gmail.com>
2021-11-30 18:33:48 +0100

    env variable

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
+require('dotenv').config({
+  path: './.env.test'
+});
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/jest/env.ts'],
 };
--- jest/utils/connection.test.ts
+++ jest/utils/connection.test.ts
@@ -1,10 +1,12 @@
 import connectToDB from "../../src/utils/connectToDB";
+jest.useFakeTimers();
 
 describe("Connection", () => {
   it("should connect", () => {
     const callback = () => {
       console.log('true')
     }
-    expect(connectToDB("mongodb://root:root@127.0.0.1:27017/test?authSource=test", callback)).toBe(undefined);
+    
+    expect(connectToDB(process.env.DATABASE_URL!, callback)).toBe(undefined);
   });
 })
